3476538c087e7c5a4d27c4a5921c24d970fcb71c
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2023-02-19 18:20:24 -0500

    initial welcome

--- frontend/coffeechat/src/routes/welcome.jsx
+++ frontend/coffeechat/src/routes/welcome.jsx
@@ -3,9 +3,20 @@ import './styles/welcome.css'
 
 function Welcome() {
     return(
-        // This is the welcome page.
         <div>
-            <h1>Welcome to Coffeechat</h1>
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Work+Sans:wght@500&display=swap" rel="stylesheet"/>
+            </head>
+            {/* This is the welcome page. */}
+            <div className="welcome-container">
+                <p className="welcome-title">Welcome to Coffeechat</p>
+                <p className="subtitle">Find your newest friendship today</p>
+                <Link to="/signup">
+                    <button className="signup-button">Join</button>
+                </Link>
+            </div>
         </div>
     );
 }
