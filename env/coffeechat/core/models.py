2f9a1e17b1f2180ade72bfef736bd4323f220500
nick-cirillo <77818146+nick-cirillo@users.noreply.github.com>
2023-04-02 17:40:41 -0400

    added some fields to user model

--- env/coffeechat/core/models.py
+++ env/coffeechat/core/models.py
@@ -1,27 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(blank=True)
+    YEAR_CHOICES = [
+        ('FR', 'Freshman'),
+        ('SO', 'Sophomore'),
+        ('JR', 'Junior'),
+        ('SR', 'Senior'),
+        ('GR', 'Graduate'),
+    ]
+    year = models.CharField(max_length=2, choices=YEAR_CHOICES, null=True)
 
 class React(models.Model):
-    email = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    desc = models.TextField(max_length=3000)
-    # image = models.ImageField(upload_to=".\images")
+    blankField = models.CharField(max_length=1)
+    # email = models.CharField(max_length=100)
+    # name = models.CharField(max_length=100)
+    # desc = models.TextField(max_length=3000)
+    # # image = models.ImageField(upload_to=".\images")
     
-    FRESHMAN = 'Freshman'
-    SOPHOMORE = 'Sophomore'
-    JUNIOR = 'Junior'
-    SENIOR = 'Senior'
-    GRADUATE = 'Graduate Student'
-    YEAR_CHOICES = [
-        (FRESHMAN, 'Freshman'),
-        (SOPHOMORE, 'Sophomore'),
-        (JUNIOR, 'Junior'),
-        (SENIOR, 'Senior'),
-        (GRADUATE, 'Graduate'),
-    ]
-    year = models.CharField(
-        max_length=16,
-        choices=YEAR_CHOICES,
-        default=FRESHMAN,
-    )
+    # FRESHMAN = 'Freshman'
+    # SOPHOMORE = 'Sophomore'
+    # JUNIOR = 'Junior'
+    # SENIOR = 'Senior'
+    # GRADUATE = 'Graduate Student'
+    # YEAR_CHOICES = [
+    #     (FRESHMAN, 'Freshman'),
+    #     (SOPHOMORE, 'Sophomore'),
+    #     (JUNIOR, 'Junior'),
+    #     (SENIOR, 'Senior'),
+    #     (GRADUATE, 'Graduate'),
+    # ]
+    # year = models.CharField(
+    #     max_length=16,
+    #     choices=YEAR_CHOICES,
+    #     default=FRESHMAN,
+    # )
